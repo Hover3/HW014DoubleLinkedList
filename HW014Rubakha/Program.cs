ca8c799c87981dfcf5ec0880352fb57189277de6
DellM6500\Пользователь <yurij.osbb@gmail.com>
2021-05-13 15:16:15 +0300

    Renamed files and folders to HW014

--- HW014Rubakha/Program.cs
+++ HW014Rubakha/Program.cs
@@ -10,6 +10,12 @@ namespace HW014
             Console.WriteLine("HW14 Double linked list");
             MyDoubleLinkedList myList = new MyDoubleLinkedList();
             Console.WriteLine($"List item count: {myList.count}");
+            Console.WriteLine($"Adding item 0 at the begin");
+            myList.AddFirst(0);
+
+            Console.WriteLine($"Adding item 10 at the end");
+            myList.AddLast(10);
+
 
 
         }
