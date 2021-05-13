e0bec8509dfc71f494a3fef4002abbf8db1de923
DellM6500\Пользователь <yurij.osbb@gmail.com>
2021-05-13 12:09:46 +0300

    Added class MyListNode

---
+++ HW014Rubakha/Implementations/MyListNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HW014Rubakha.Interfaces;
+
+namespace HW014Rubakha.Implementations
+{
+    class MyListNode : IListNode
+    {
+        
+        IListNode IListNode.Next 
+        { get; set ; }
+        IListNode IListNode.Previous 
+        { get; set; }
+        int IListNode.Data 
+        { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
